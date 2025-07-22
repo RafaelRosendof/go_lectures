7b9f2f87f24404ec5b9f8bff0a9ef12ffe7d5bcf
Rafael <rafael.19.rosendo@gmail.com>
2025-07-22 10:05:04 -0300

    testes automatizados feitos

--- redBlack/run_terminal/run_main.go
+++ redBlack/run_terminal/run_main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 
-	runMain(10)
+	runMain(20)
 }
 
 func runMain(number_times int) {
--- redBlack/test/redBlack_test.go
+++ redBlack/test/redBlack_test.go
@@ -1,9 +1,32 @@
 package test
 
 import (
-	"redblack/tree"
+	"bytes"
+	"fmt"
+	"log"
+	"os/exec"
+	"redBlack/tree"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
+func Remove_csv(csv_in string) {
+	cmd := exec.Command("rm", csv_in)
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(out.String())
+
+	fmt.Println("Done, we deleted the csv")
+}
+
+/*
 func TestRoot(t *testing.T) {
 
 	tests := []struct {
@@ -32,30 +55,126 @@ func TestRoot(t *testing.T) {
 
 }
 
+*/
+
 func TestInsert100(t *testing.T) {
+
+	lines_in := 110
+	csv_in := "teste_100.csv"
+
+	tree.Write_csv(csv_in, lines_in)
+
+	fmt.Println("Creating csv for 100 test")
+	root := tree.Read_csv(nil, csv_in)
+
+	is_valid := tree.IsValid(root)
+
+	//Remove_csv(csv_in)
+	assert.True(t, is_valid, "Tree is balanced and tested ok")
 
 }
 
 func TestInsert1_000_000(t *testing.T) {
 
+	lines_in := 1_000_010
+	csv_in := "test_1_000_000.csv"
+
+	tree.Write_csv(csv_in, lines_in)
+
+	fmt.Println("Creating csv for 1_000_000")
+	root := tree.Read_csv(nil, csv_in)
+
+	is_valid := tree.IsValid(root)
+
+	//Remove_csv(csv_in)
+
+	assert.True(t, is_valid, "tree is balanced for 1_000_000")
+
 }
 
 func TestInsert1_000_000_000(t *testing.T) {
 
+	lines_in := 100_000_010
+	csv_in := "test_1_000_000_000.csv"
+
+	tree.Write_csv(csv_in, lines_in)
+
+	fmt.Println("Creating csv for 1_000_000_000")
+	root := tree.Read_csv(nil, csv_in)
+
+	is_valid := tree.IsValid(root)
+
+	//Remove_csv(csv_in)
+
+	assert.True(t, is_valid, "tree is balanced for 1_000_000_000")
 }
 
 func TestRemove100(t *testing.T) {
 
+	root := tree.Read_csv(nil, "teste_100.csv")
+
+	fmt.Println("Testando remoção de 100 itens")
+
+	remove_numbers := tree.Remove_nodes(100, "teste_100.csv")
+	for i := 0; i < len(remove_numbers); i++ {
+
+		if tree.Busca_no_raiz(root, remove_numbers[i]) == false {
+			continue
+		}
+
+		tree.Arv_removeRB(root, remove_numbers[i])
+	}
+
+	is_valid := tree.IsValid(root)
+
+	Remove_csv("teste_100.csv")
+	assert.True(t, is_valid, "Tree is balanced after 100 removes")
 }
 
+/*
 func TestRemove1_000_000(t *testing.T) {
 
+	root := tree.Read_csv(nil, "test_1_000_000.csv")
+
+	fmt.Println("Testando remoção de 1_000_000 itens")
+
+	remove_numbers := tree.Remove_nodes(500_000, "test_1_000_000.csv")
+	for i := 0; i < len(remove_numbers); i++ {
+		//fmt.Println("Attempting to remove:", remove_numbers[i])
+		if tree.Busca_no_raiz(root, remove_numbers[i]) == false {
+			continue
+		}
+
+		tree.Arv_removeRB(root, remove_numbers[i])
+	}
+
+	is_valid := tree.IsValid(root)
+
+	Remove_csv("teste_1_000_000.csv")
+	assert.True(t, is_valid, "Tree is balanced after 1_000_000 removes")
+
 }
+*/
 
-func ItsBalanced(t *testing.T) {
+func TestRemove1_000_000_000(t *testing.T) {
 
-}
+	root := tree.Read_csv(nil, "test_1_000_000_000.csv")
 
-func TestInsertAndFindAndDelete(t *testing.T) {
+	fmt.Println("Testando remoção de 1_000_000_000 itens")
+
+	remove_numbers := tree.Remove_nodes(100_000, "test_1_000_000_000.csv")
+	for i := 0; i < len(remove_numbers); i++ {
+
+		if tree.Busca_no_raiz(root, remove_numbers[i]) == false {
+			continue
+		}
+
+		tree.Arv_removeRB(root, remove_numbers[i])
+	}
+
+	is_valid := tree.IsValid(root)
+
+	Remove_csv("test_1_000_000_000.csv")
+	assert.True(t, is_valid, "Tree is balanced after 1_000_000_000 removes")
 
 }
